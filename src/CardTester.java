c3b0f442a1064cc04027c744af4f95160b78c94b
duynguyen2 <thenman233@gmail.com>
2017-03-14 09:09:13 -0700

    third commit

--- src/CardTester.java
+++ src/CardTester.java
@@ -12,6 +12,8 @@ public class CardTester {
 		Card card1 = new Card("10", "Spades", 10);
 		Card card2 = new Card("King", "Hearts", 11);
 		Card card3 = new Card("Ace", "Diamonds", 1);
+		System.out.println(card1.matches(card2));
+		System.out.println(card2.matches(card3));
 		System.out.println(card1.toString());
 		System.out.println(card2.toString());
 		System.out.println(card3.toString());
